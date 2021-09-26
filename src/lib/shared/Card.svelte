137bfa4e0b1f7d5ad29a3dea4bb91b383c0189a4
Hein Dauven <heindauven@gmail.com>
2021-09-27 00:16:55 +0200

    Add container card component.

---
+++ src/lib/shared/Card.svelte
@@ -0,0 +1,16 @@
+<div class="card">
+    <slot />
+</div>
+
+<style>
+    .card {
+        display: flex;
+        flex-direction: column;
+        background: var(--card-color);
+        padding: 2em;
+        border-radius: 12px;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+        transition: background-color 0.2s linear;
+        width: 100%;
+    }
+</style>
