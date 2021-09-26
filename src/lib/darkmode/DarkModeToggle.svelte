fad2c6b1d16ea8daa770cbdf1fee2f98a9430815
Hein Dauven <heindauven@gmail.com>
2021-09-27 00:15:38 +0200

    Add darkmode toggle component.

---
+++ src/lib/darkmode/DarkModeToggle.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from "svelte";
+    import { mode, toggleMode, setMode } from "../../stores/darkmode";
+    import lightToggle from "./light-toggle.svg";
+    import darkToggle from "./dark-toggle.svg";
+
+    $: modeIcon = $mode === "dark" ? darkToggle : lightToggle;
+
+    onMount(() => {
+        setMode();
+    });
+</script>
+
+<button on:click={toggleMode}>
+    <img src={modeIcon} alt="darkmode toggle" class="toggle" />
+</button>
+
+<style>
+    button {
+        border: 0;
+        background-color: transparent;
+        cursor: pointer;
+        font-size: 16px;
+        width: 3em;
+        height: 3em;
+        padding: 0;
+    }
+
+    .toggle {
+        width: 3em;
+        height: 3em;
+    }
+</style>
