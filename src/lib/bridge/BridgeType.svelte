d475afe61b152252bb08c8ae720ff984cd4d557d
Hein Dauven <heindauven@gmail.com>
2021-09-28 09:13:42 +0200

    Add component to switch between deposit and withdrawal. (Needs L1 <- L2> switching request).

---
+++ src/lib/bridge/BridgeType.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let deposit = true;
+
+    function toggleTab() {
+        deposit = !deposit;
+    }
+</script>
+
+<ul>
+    <li class="{deposit ? 'active' : ''}" on:click={() => {if (!deposit) toggleTab()}}>Deposit</li>
+    <li class="{!deposit ? 'active' : ''}" on:click={() => {if (deposit) toggleTab()}}>Withdraw</li>
+</ul>
+
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+        margin: 0 0 0.5rem 0;
+        padding: 0;
+    }
+
+    li {
+        padding: 0 1rem 1rem 1rem;
+        font-size: 20px;
+        font-weight: 500;
+        color: var(--text-color-medium-grey);
+        cursor: pointer;
+    }
+
+    .active {
+        background: -webkit-linear-gradient(45deg, #ff44c9 0%, #00b8fa 100%)
+            left bottom;
+        background-size: 100% 5px;
+        background-clip: unset;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-align: center;
+
+        border-bottom: 3px solid;
+        border-image-slice: 1;
+        border-image-source: linear-gradient(to left, #00b8fa 0%, #ff44c9 100%);
+        border-radius: 3px;
+    }
+
+    li:first-child {
+        margin-right: 1rem;
+    }
+</style>
