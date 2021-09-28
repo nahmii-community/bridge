f2678fbf3948d25a05ea0a1a19a9c146792ce085
Hein Dauven <heindauven@gmail.com>
2021-09-28 09:03:56 +0200

    Add index page. Switches between connection and bridge component on connection.

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+    import ConnectWallet from "$lib/ConnectWallet.svelte";
+    import Bridge from "$lib/bridge/Bridge.svelte"
+    import { isConnected } from "../stores/wallet";
+
+    let connected = false;
+
+    isConnected.subscribe(value => {
+        connected = value;
+    })
+</script>
+
+{#if connected} 
+    <Bridge />
+{:else}
+    <ConnectWallet />
+{/if}
