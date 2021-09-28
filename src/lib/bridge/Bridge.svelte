afddf16c9fd4eb3d4fa130f2cf7bde13acd9f9d1
Hein Dauven <heindauven@gmail.com>
2021-09-28 09:17:42 +0200

    Add composed component that forms the bridge view.

---
+++ src/lib/bridge/Bridge.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Divider from "$lib/shared/Divider.svelte";
+    import Button from "$lib/shared/Button.svelte";
+    import Card from "../shared/Card.svelte";
+    import BridgeType from "./BridgeType.svelte";
+    import To from "./To.svelte";
+    import From from "./From.svelte";
+
+    let deposit = true;
+    $: buttonText = deposit === true ? "DEPOSIT" : "WITHDRAW";
+</script>
+
+<div class="container">
+    <Card>
+        <BridgeType bind:deposit />
+        <From />
+        <Divider />
+        <To />
+        <Button>{buttonText}</Button>
+    </Card>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        max-width: 550px;
+        margin: auto;
+        justify-content: center;
+    }
+</style>
