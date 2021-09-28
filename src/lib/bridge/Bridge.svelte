de5f11b4ed59bfab9d93e20a9122fe4a39a09e09
Hein Dauven <heindauven@gmail.com>
2021-09-28 13:04:35 +0200

    Add logic to switch between deposit <-> withdrawal, based on the L1 <-> L2 relation.

--- src/lib/bridge/Bridge.svelte
+++ src/lib/bridge/Bridge.svelte
@@ -1,21 +1,36 @@
 <script>
+    import { onMount } from "svelte";
     import Divider from "$lib/shared/Divider.svelte";
     import Button from "$lib/shared/Button.svelte";
     import Card from "../shared/Card.svelte";
     import BridgeType from "./BridgeType.svelte";
     import To from "./To.svelte";
     import From from "./From.svelte";
+    import { network, supportedNetworks } from "../../stores/wallet";
 
-    let deposit = true;
+    let networkDetails;
+    let activeNetwork;
+    let companionNetwork;
+    let L2;
+
+    let deposit;
     $: buttonText = deposit === true ? "DEPOSIT" : "WITHDRAW";
+    $: deposit = L2 === false ? true : false;
+
+    network.subscribe(async (value) => {
+        networkDetails = await supportedNetworks(value);
+        activeNetwork = networkDetails.chainName;
+        companionNetwork = networkDetails.companionChainName;
+        L2 = networkDetails.L2;
+    });
 </script>
 
 <div class="container">
     <Card>
         <BridgeType bind:deposit />
-        <From />
+        <From network={activeNetwork} />
         <Divider />
-        <To />
+        <To network={companionNetwork} />
         <Button>{buttonText}</Button>
     </Card>
 </div>
