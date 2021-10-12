85f25c8b740d4cdd1ce0c3339017eb1fca39e7df
Hein <heindauven@gmail.com>
2021-10-12 23:38:17 +0200

    Add Github pages compatible static resources routing configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,19 @@
 import adapter from "@sveltejs/adapter-static";
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-        adapter: adapter({
-            // default options are shown
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-        }),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		paths: {
-			// base: "/gateway",
+			base: dev ? '' : '/gateway',
 		},
 	}
 };
