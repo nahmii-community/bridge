718a22fee313a943466232ff25b2cd246663a68e
Hein <heindauven@gmail.com>
2021-10-13 12:54:22 +0200

    Add Toast container to the main page. Make toasts pausible by default, increase visible duration.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
 <script>
+    import { SvelteToast } from "@zerodevx/svelte-toast";
     import ConnectWallet from "$lib/ConnectWallet.svelte";
     import Bridge from "$lib/bridge/Bridge.svelte"
     import { isConnected } from "../stores/wallet";
 
     let connected = false;
+
+    const options = {
+        pausable: true,
+        duration: 10000
+    };
 
     isConnected.subscribe(value => {
         connected = value;
@@ -15,3 +21,5 @@
 {:else}
     <ConnectWallet />
 {/if}
+
+<SvelteToast {options} />
