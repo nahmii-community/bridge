69b1bfab063f406c83a540509721b663d611bc6a
Hein <heindauven@gmail.com>
2021-10-19 00:31:15 +0200

    Add ripple effect on button click.

--- src/lib/shared/Button.svelte
+++ src/lib/shared/Button.svelte
@@ -8,9 +8,20 @@
 
     const dispatch = createEventDispatcher();
 
-    function onClick() {
+    function applyRipple(e) {
+        const ripple = document.createElement("div");
+        ripple.className = "ripple";
+        document.body.appendChild(ripple);
+        ripple.style.left = `${e.clientX}px`;
+        ripple.style.top = `${e.clientY}px`;
+        ripple.style.animation = "ripple-effect 400ms linear";
+        ripple.onanimationend = () => document.body.removeChild(ripple);
+    }
+
+    function onClick(e) {
         if (!wait) {
             wait = true;
+            applyRipple(e);
             clearTimeout(timer);
             timer = setTimeout(() => {
                 wait = false;
@@ -26,20 +37,29 @@
 
 <style>
     button {
-        background: -webkit-linear-gradient(45deg, #ff44c9 0%, #00b8fa 100%);
+        background: linear-gradient(
+            45deg,
+            rgba(255, 68, 201, 1) 0%,
+            rgba(0, 184, 250, 1) 100%
+        );
         border-radius: 6px;
         font: 500 16px Roboto;
         color: var(--text-color-inverse);
         height: 56px;
         border-style: none;
-        transition: opacity 400ms;
+        transition: all 400ms;
     }
 
     button:hover {
-        opacity: 0.6;
+        opacity: 0.7;
     }
 
     button:disabled {
         opacity: 0.4;
+    }
+
+    button:active {
+        opacity: 1;
+        transition: all 400ms;
     }
 </style>
