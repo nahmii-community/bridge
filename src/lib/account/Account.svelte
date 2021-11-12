18b9374f3ad7193077a7cfcadc849e412c54c411
Hein <heindauven@gmail.com>
2021-11-12 11:51:08 +0100

    Add account history page.

---
+++ src/lib/account/Account.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Card from "$lib/shared/Card.svelte";
+</script>
+
+<div class="container">
+    <Card>account history</Card>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        max-width: 550px;
+        margin: auto;
+        justify-content: center;
+    }
+</style>
