18317c02537df712ab14d815f7f93c20574d6e9a
Hein <heindauven@gmail.com>
2021-11-20 16:38:09 +0100

    Move account withdrawal and deposit displaying into a separate component.

--- src/lib/account/Account.svelte
+++ src/lib/account/Account.svelte
@@ -1,9 +1,31 @@
 <script>
     import Card from "$lib/shared/Card.svelte";
+    import TransactionTable from "$lib/account/TransactionTable.svelte";
+    import GradientTitle from "$lib/shared/GradientTitle.svelte";
+    import { onMount } from "svelte";
+
+    let withdrawals = [];
+    let deposits = [];
+
+    onMount(() => {
+        // TODO Check active account
+        // TODO fetch deposits and withdrawals from localStorage for active account
+    });
 </script>
 
 <div class="container">
-    <Card>account history</Card>
+    <Card>
+        <GradientTitle>Account History</GradientTitle>
+
+        <p>Recent Withdrawals</p>
+        <TransactionTable
+            transactions={withdrawals}
+            transactionType="withdrawals"
+        />
+
+        <p>Recent Deposits</p>
+        <TransactionTable transactions={deposits} transactionType="deposits" />
+    </Card>
 </div>
 
 <style>
