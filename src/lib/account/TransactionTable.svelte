56aa9cd43e37293291f8451006a747b837ff0383
Hein <heindauven@gmail.com>
2021-11-20 12:39:17 +0100

    Add table component to format and display deposit and withdrawal information.

---
+++ src/lib/account/TransactionTable.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let transactions = [];
+    export let transactionType = "transaction";
+</script>
+
+{#if transactions.length != 0}
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Hash</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each transactions as transaction}
+                <tr>
+                    <td>{transaction.timestamp}</td>
+                    <td>{transaction.hash}</td>
+                    <td>{transaction.status}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+{:else}
+    No recent {transactionType}
+{/if}
