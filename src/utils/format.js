b783d891827640f09fc179a2876314dd48048fa9
Hein <heindauven@gmail.com>
2021-11-20 14:01:24 +0100

    Extend shorten format function with default and settable start and end points. Add docs.

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,8 +1,18 @@
-export const shorten = (address) => {
-    let shortAddress = "";
-    return shortAddress.concat(
-        address.slice(0, 7),
+/**
+ * Shortens a given hash/string. 
+ * 
+ * It takes the first n characters and the last n characters,
+ * and replaces all other characters with 3 dots.
+ * 
+ * @param {string} input - The value to shorten.
+ * @param {number} [start=7] - The first n characters to take from the string. (Default: 7)
+ * @param {number} [end=5] - The last n characters to take from the string. (Default: 5)
+ */
+export const shorten = (input, start = 7, end = 5) => {
+    let shortenedInput = "";
+    return shortenedInput.concat(
+        input.slice(0, start),
         "...",
-        address.slice(-5)
+        input.slice(-Math.abs(end))
     );
 }
