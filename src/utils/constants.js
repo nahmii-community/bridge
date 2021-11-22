c413c7cf51674ed45577f3643f4386b92e4c4814
Hein <heindauven@gmail.com>
2021-11-23 00:07:49 +0100

    Add a constants util file.

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+import { BigNumber } from "ethers";
+
+export const DEFAULT_GAS_L1 = 330_000;
+export const DEFAULT_GAS_L2 = 1_300_000;
+export const L2_STANDARD_BRIDGE = "0x4200000000000000000000000000000000000010";
+export const MAX_APPROVAL_AMOUNT = BigNumber.from(
+    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+); // Unlimited approval
+export const NVM_ETH = "0x4200000000000000000000000000000000000006";
+export const WARNING_L2_ETH_BALANCE = BigNumber.from("10000000000000000"); // 0.01 ETH
+export const ZERO = BigNumber.from("0");
