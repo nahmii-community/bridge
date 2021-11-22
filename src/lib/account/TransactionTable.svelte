ed1f6d4505e0a2a0cb99bd28c406a8c20afe0490
Hein <heindauven@gmail.com>
2021-11-22 21:24:35 +0100

    Update TransactionTable styling to properly deal with smaller screens.

--- src/lib/account/TransactionTable.svelte
+++ src/lib/account/TransactionTable.svelte
@@ -19,7 +19,7 @@
                 {#each transactions as transaction}
                     <tr>
                         <td>{timestampToDateTime(transaction.timestamp)}</td>
-                        <td>{shorten(transaction.hash)}</td>
+                        <td>{shorten(transaction.hash, 4, 3)}</td>
                         <td>{transaction.status}</td>
                     </tr>
                 {/each}
@@ -32,14 +32,43 @@
 
 <style>
     .container {
-        margin-bottom: 1em;
+        margin-bottom: 1.5em;
     }
 
     .container:last-of-type {
         margin-bottom: 0;
     }
 
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
     th {
         text-align: left;
+        padding: 0.5em 0;
+    }
+
+    tr {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    }
+
+    td {
+        font-size: 0.8em;
+        padding: 0.5em 0.1em 0.5em 0;
+    }
+
+    @media (min-width: 28em) {
+        td {
+            font-size: 0.9em;
+            padding: 0.5em 0.2em 0.5em 0;
+        }
+    }
+
+    @media (min-width: 32em) {
+        td {
+            font-size: 1em;
+            padding: 0.5em 0.3em 0.5em 0;
+        }
     }
 </style>
