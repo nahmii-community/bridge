044fca3df895dede3ce6ca10d6728e60463592b2
Hein <heindauven@gmail.com>
2021-11-23 17:21:19 +0100

    Add a general store transaction util function that handles the storing of general transactions for a user based on their wallet address and the chainId the transaction is executed on.

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -20,3 +20,32 @@ export const getTransactions = (chainId, wallet) => {
         withdrawals,
     }
 }
+
+export const storeTransaction = (chainId, wallet, token, transaction, status, type) => {
+    const networks = get(wallet.toLowerCase(), null);
+    
+    if (networks && networks.hasOwnProperty(chainId)) {
+        const network = networks[chainId];
+        if (network.hasOwnProperty(type)) {
+            network[type].push({
+                hash: transaction.hash,
+                status,
+                timestamp: transaction.timestamp,
+                token
+            });
+        }
+    } else {
+        networks[chainId] = {
+            withdrawals: [],
+            deposits: [],
+        };
+        networks[chainId][type].push({
+            hash: transaction.hash,
+            status,
+            timestamp: transaction.timestamp,
+            token
+        });
+    }
+    
+    set(wallet.toLowerCase(), networks);
+}
