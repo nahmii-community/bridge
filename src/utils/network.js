08cd52a300f5cd71eb6dce9145f6f2bd4da913a5
Hein <heindauven@gmail.com>
2021-11-24 02:54:25 +0100

    Add fraud proof window helper function.

--- src/utils/network.js
+++ src/utils/network.js
@@ -6,3 +6,10 @@ export const getExplorerUrl = (chainId) => {
     });
     return networkInfo.blockExplorerUrls[0];
 }
+
+export const getFraudProofWindow = (chainId) => {
+    let networkInfo = SUPPORTED_NETWORKS.find(val => {
+        return val.chainId == chainId;
+    });
+    return networkInfo.fraudProofWindow;
+}
