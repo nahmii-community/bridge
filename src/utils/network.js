a25a92fd06389e2350fbd700df919087c6d22caf
Hein <heindauven@gmail.com>
2021-12-22 18:42:01 +0100

    Document network util functions.

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,12 +1,25 @@
 import { SUPPORTED_NETWORKS } from "./constants";
 
+/**
+ * Retrieves the first block explorer URL for a given network.
+ * 
+ * @param {string} chainId Hexadecimal string of the chain to find the block explorer URL for.
+ * @returns Block explorer URL.
+ */
 export const getExplorerUrl = (chainId) => {
     let networkInfo = SUPPORTED_NETWORKS.find(val => {
         return val.chainId == chainId;
     });
+    // TODO: Add error handling and/or return array of block explorer URLs.
     return networkInfo.blockExplorerUrls[0];
 }
 
+/**
+ * Finds the fraud proof window for a given network.
+ * 
+ * @param {string} chainId Hexadecimal string of the chain to find the fraud proof window for.
+ * @returns The fraud proof window in seconds.
+ */
 export const getFraudProofWindow = (chainId) => {
     let networkInfo = SUPPORTED_NETWORKS.find(val => {
         return val.chainId == chainId;
@@ -14,6 +27,12 @@ export const getFraudProofWindow = (chainId) => {
     return networkInfo.fraudProofWindow;
 }
 
+/**
+ * Finds the companion network for a given network.
+ * 
+ * @param {string} chainId Hexadecimal string of the chain to find the companion chain for.
+ * @returns Object of the companion networks metadata.
+ */
 export const findCompanionNetwork = async (chainId) => {
     let networkInfo = SUPPORTED_NETWORKS.find(val => {
         return val.chainId == chainId;
