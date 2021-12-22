7684c19cacfbce9946707b2389013313fb4fc51a
Hein <heindauven@gmail.com>
2021-12-22 18:26:39 +0100

    Add function to find a companion network (L1 or L2) for a given chain ID.

--- src/utils/network.js
+++ src/utils/network.js
@@ -13,3 +13,20 @@ export const getFraudProofWindow = (chainId) => {
     });
     return networkInfo.fraudProofWindow;
 }
+
+export const findCompanionNetwork = async (chainId) => {
+    let networkInfo = SUPPORTED_NETWORKS.find(val => {
+        return val.chainId == chainId;
+    });
+    let companionNetworkInfo = SUPPORTED_NETWORKS.find(val => {
+        return val.chainId == networkInfo.companionChainId;
+    })
+    if (!networkInfo) {
+        networkInfo = {
+            chainId,
+            chainName: "Unsupported",
+            isSupported: false
+        }
+    }
+    return companionNetworkInfo;
+}
