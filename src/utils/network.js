8677c88ebcbe7e0bb958a7014579e4f035130abb
Hein <heindauven@gmail.com>
2021-12-22 23:49:40 +0100

    Add umbralla network term function to network utils.

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,9 +1,24 @@
 import { SUPPORTED_NETWORKS } from "./constants";
+import { L1L2_NETWORKS } from "./constants";
+
+/**
+ * Finds the umbrella term used for a set of networks based on the chain ID.
+ * 
+ * @param {string} chainId Hexadecimal representation of the chain ID for a given network to find the umbrella term for.
+ * @returns The umbrella term for a given network.
+ */
+export const findUmbrellaNetwork = (chainId) => {
+    for (let network in L1L2_NETWORKS) {
+        if (L1L2_NETWORKS[network].includes(chainId.toLowerCase())) {
+            return network;
+        }
+    }
+}
 
 /**
  * Retrieves the first block explorer URL for a given network.
  * 
- * @param {string} chainId Hexadecimal string of the chain to find the block explorer URL for.
+ * @param {string} chainId Hexadecimal representation of the chain ID for a given network to find the block explorer URL for.
  * @returns Block explorer URL.
  */
 export const getExplorerUrl = (chainId) => {
@@ -17,7 +32,7 @@ export const getExplorerUrl = (chainId) => {
 /**
  * Finds the fraud proof window for a given network.
  * 
- * @param {string} chainId Hexadecimal string of the chain to find the fraud proof window for.
+ * @param {string} chainId Hexadecimal representation of the chain ID for a given network to find the fraud proof window for.
  * @returns The fraud proof window in seconds.
  */
 export const getFraudProofWindow = (chainId) => {
@@ -29,7 +44,7 @@ export const getFraudProofWindow = (chainId) => {
 
 /**
  * 
- * @param {string} chainId Hexadecimal string of the chain to find all metadata for.
+ * @param {string} chainId Hexadecimal representation of the chain ID for a given network to find all metadata for.
  * @returns All metadata for a given network if the network is available, 
  *          else return an array stating the network is unsupported.
  */
@@ -50,7 +65,7 @@ export const findSupportedNetwork = async (chainId) => {
 /**
  * Finds the companion network for a given network.
  * 
- * @param {string} chainId Hexadecimal string of the chain to find the companion chain for.
+ * @param {string} chainId Hexadecimal representation of the chain ID for a given network to find the companion chain for.
  * @returns Object of the companion networks metadata.
  */
 export const findCompanionNetwork = async (chainId) => {
