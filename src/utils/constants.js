8a229da391995ceac79191500e5a7be36a78bdc2
Hein <heindauven@gmail.com>
2022-01-14 00:18:38 +0100

    remove unused constants.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,7 @@
-export const DEFAULT_GAS_L1 = 330_000;
-export const DEFAULT_GAS_L2 = 1_300_000;
 export const L1L2_NETWORKS = {
     mainnet: ["0x1", "0x15af"],
     testnet: ["0x3", "0x15b1"]
 };
-export const L2_STANDARD_BRIDGE = "0x4200000000000000000000000000000000000010";
 export const MAX_APPROVAL_AMOUNT = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"; // Unlimited approval
 export const NVM_ETH = "0x4200000000000000000000000000000000000006";
 export const WARNING_L2_ETH_BALANCE = "10000000000000000"; // 0.01 ETH
