acc09caf78f0054d62bac6dbde7d77a3490fb41d
Hein <heindauven@gmail.com>
2022-08-03 09:31:24 +0200

    Update constants to target Goerli and OP testnet.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
 export const DEPOSIT_ETH_GAS_LIMIT = "330000";
 export const DEPOSIT_ERC20_GAS_LIMIT = "375000";
 export const L1L2_NETWORKS = {
-    mainnet: ["0x1", "0x15af"],
-    testnet: ["0x3", "0x15b1"]
+    testnet: ["0x5", "0x1A4"]
 };
 export const MAX_APPROVAL_AMOUNT = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"; // Unlimited approval
 export const NVM_ETH = "0x4200000000000000000000000000000000000006";
@@ -11,47 +10,25 @@ export const ZERO = "0";
 
 export const SUPPORTED_NETWORKS = [
     {
-        chainId: "0x1",
-        chainName: "Ethereum Mainnet",
+        chainId: "0x5",
+        chainName: "Goerli Testnet",
         isSupported: true,
         L2: false,
-        rpcUrls: ["https://mainnet.infura.io/v3/f36942ff00514b7fb154a652fa510972"],
-        blockExplorerUrls: ["https://etherscan.io"],
-        companionChainId: "0x15af",
-        standardBridge: "0x2fCE9b92a64c1DDf14a1A9E5Ec6D4e4C7C9F4Fdd",
-        crossDomainMessenger: "0x01dF38E20738c58aF8141504aa6C88013d3D6C5A",
-        fraudProofWindow: 604800
-    },
-    {
-        chainId: "0x3",
-        chainName: "Ropsten Testnet",
-        isSupported: true,
-        L2: false,
-        rpcUrls: ["https://ropsten.infura.io/v3/f36942ff00514b7fb154a652fa510972"],
-        blockExplorerUrls: ["https://ropsten.etherscan.io"],
-        companionChainId: "0x15b1",
-        standardBridge: "0x21De2607E90edb1736bc460a4cd58c0FCd74ABcc",
-        crossDomainMessenger: "0x5401Ba2f9123f4019be76fca1D0B765Fd00138De",
+        rpcUrls: ["https://goerli.infura.io/v3/74e02b65ab2d43c0a1c0cb45742fc7d7"],
+        blockExplorerUrls: ["https://goerli.etherscan.io"],
+        companionChainId: "0x1A4",
+        standardBridge: "0x636Af16bf2f682dD3109e60102b8E1A089FedAa8",
+        crossDomainMessenger: "0x5086d1eEF304eb5284A0f6720f79403b4e9bE294",
         fraudProofWindow: 360
     },
     {
-        chainId: "0x15af",
-        chainName: "Nahmii Mainnet",
+        chainId: "0x1A4",
+        chainName: "Optimism Testnet",
         isSupported: true,
         L2: true,
-        companionChainId: "0x1",
-        rpcUrls: ["https://l2.nahmii.io"],
-        blockExplorerUrls: ["https://explorer.nahmii.io"],
-        fraudProofWindow: 604800
-    },
-    {
-        chainId: "0x15b1",
-        chainName: "Nahmii Testnet",
-        isSupported: true,
-        L2: true,
-        companionChainId: "0x3",
-        rpcUrls: ["https://l2.testnet.nahmii.io"],
-        blockExplorerUrls: ["https://explorer.testnet.nahmii.io"],
+        companionChainId: "0x5",
+        rpcUrls: ["https://optimism-goerli.infura.io/v3/74e02b65ab2d43c0a1c0cb45742fc7d7"],
+        blockExplorerUrls: ["https://goerli-optimistic.etherscan.io/"],
         fraudProofWindow: 360
     }
 ]
