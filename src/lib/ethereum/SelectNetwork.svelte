7cf567675811d3f32244e56aa3920d2b19c61fc8
menawar <sylvesternathan93@gmail.com>
2023-02-03 07:06:40 -0500

    change sigle quotes to double quotes

--- src/lib/ethereum/SelectNetwork.svelte
+++ src/lib/ethereum/SelectNetwork.svelte
@@ -1,13 +1,13 @@
 <script>
-	import { createEventDispatcher } from 'svelte';
-	import Button from '$lib/shared/Button.svelte';
-	import GradientTitle from '$lib/shared/GradientTitle.svelte';
-	import Modal from '$lib/shared/Modal.svelte';
-	import { switchNetwork } from '../../stores/wallet';
+	import { createEventDispatcher } from "svelte";
+	import Button from "$lib/shared/Button.svelte";
+	import GradientTitle from "$lib/shared/GradientTitle.svelte";
+	import Modal from "$lib/shared/Modal.svelte";
+	import { switchNetwork } from "../../stores/wallet";
 
-	export let title = 'Unsupported network';
-	export let subtext = 'Your active network is not supported!';
-	export let message = 'Switch to one of the supported networks:';
+	export let title = "Unsupported network";
+	export let subtext = "Your active network is not supported!";
+	export let message = "Switch to one of the supported networks:";
 	export let closeable = false;
 
 	const dispatch = createEventDispatcher();
@@ -15,7 +15,7 @@
 	const onNetworkClick = async (chainId) => {
 		await switchNetwork(chainId);
 		if (closeable) {
-			dispatch('close');
+			dispatch("close");
 		}
 	};
 </script>
@@ -25,13 +25,13 @@
 	<p class="modal-text">{subtext}</p>
 	<p class="modal-text">{message}</p>
 	<div class="buttons">
-		<Button on:click={() => onNetworkClick('0x1')}>Ethereum Mainnet</Button>
+		<Button on:click={() => onNetworkClick("0x1")}>Ethereum Mainnet</Button>
 		<div class="spacer" />
-		<Button on:click={() => onNetworkClick('0x15af')}>Nahmii 2 Mainnet</Button>
+		<Button on:click={() => onNetworkClick("0x15af")}>Nahmii 2 Mainnet</Button>
 		<div class="spacer" />
-		<Button on:click={() => onNetworkClick('0x5')}>Goerli Testnet</Button>
+		<Button on:click={() => onNetworkClick("0x5")}>Goerli Testnet</Button>
 		<div class="spacer" />
-		<Button on:click={() => onNetworkClick('0x15b1')}>Nahmii 2 Testnet</Button>
+		<Button on:click={() => onNetworkClick("0x15b1")}>Nahmii 2 Testnet</Button>
 	</div>
 </Modal>
 
